2b4c94e02265a80da2c218a64d480c2405e5fd87
Eazo1 <ahmadsamehhakam@gmail.com>
2025-02-23 17:39:22 +0000

    More matrices in training set.

--- seed_to_matrix.py
+++ seed_to_matrix.py
@@ -6,6 +6,7 @@ import obstruction_and_relic_matrix
 import gen_map_from_game
 import gc
 import jax
+import re
 
 
 #map_iteration_period = 20 # Can be other values (must determine in game)
@@ -45,7 +46,16 @@ plt.show()
 
 # Define function to create and save as many GOMs as needed
 def create_and_save_grand_obstruction_matrix(num_matrices):
-    for m in range(num_matrices):
+    file_location = 'C:/Users/ahmad/Desktop/lux/GOM_seed_dataset/'
+    os.makedirs(file_location, exist_ok=True)
+
+    # Find the highest existing GOM number
+    existing_files = [f for f in os.listdir(file_location) if f.startswith('grand_obstruction_matrix_') and f.endswith('.npy')]
+    existing_numbers = [int(re.search(r'(\d+)', f).group(0)) for f in existing_files if re.search(r'(\d+)', f)]
+
+    start_index = max(existing_numbers) + 1 if existing_numbers else 0  # Start from next available index
+
+    for m in range(start_index, start_index + num_matrices):
         random_seed = np.random.randint(0, 2**10)
         map, relic_nodes, params = gen_map_from_game.gen_game_map_time_start(random_seed)
         params = json.loads(params)
@@ -58,44 +68,30 @@ def create_and_save_grand_obstruction_matrix(num_matrices):
         
         for i in range(map.shape[0]):
             for j in range(map.shape[1]):
-                obstruction_matrix.set_index_value(map_iteration_period, time_iteration=0, iteration_index=[i,j], discrete_values=map[i,j])
+                obstruction_matrix.set_index_value(map_iteration_period, time_iteration=0, iteration_index=[i, j], discrete_values=map[i, j])
 
-        #print(type(obstruction_matrix.data[0][0]))  # Check the data type of one cell
-        #print(obstruction_matrix.data[0][0])       # Print its contents
-
-        
         obstruction_matrix_data = obstruction_matrix.data.astype(int)
 
-        #obstruction_matrix_data = np.array([[np.mean(cell) for cell in row] for row in obstruction_matrix.data], dtype=float)
-        #plt.imshow(obstruction_matrix_data, cmap='viridis')
-        #plt.savefig('grand_obstruction_matrix_' + str(i) + '.png')
-        #plt.close()
-        
-        
-        # set the location for the obstruction matrix saves to go
-        file_location = 'C:/Users/ahmad/Desktop/lux/GOM_seed_dataset/'
-        os.makedirs(file_location, exist_ok=True)
-
-        # save the obstruction matrix as a numpy file
-        np.save(file_location + 'grand_obstruction_matrix_' + str(m), obstruction_matrix_data)
+        # Save the obstruction matrix as a numpy file
+        np.save(os.path.join(file_location, f'grand_obstruction_matrix_{m}.npy'), obstruction_matrix_data)
         
         del obstruction_matrix, obstruction_matrix_data, map, relic_nodes, params
         gc.collect()  # Run garbage collection to free up memory
-        jax.clear_caches()  # Clear JAX backends to free up memory
+        jax.clear_caches()  # Clear JAX backend caches
         
-        #print('Grand Obstruction Matrix ' + str(m) + ' saved successfully!')
-        # Update the user on the progress
-        if m % 50 == 0:
-            print(str(m) + ' Grand Obstruction Matrices saved!')
+        if (m - start_index) % 50 == 0:
+            print(f'{m - start_index} Grand Obstruction Matrices saved!')
+
+    print(f'Completed saving {num_matrices} new Grand Obstruction Matrices starting from index {start_index}.')
 
 # Check if there are already GOMs saved in the directory
 # If there are, delete them
-file_location = 'C:/Users/ahmad/Desktop/lux/GOM_seed_dataset/'
+'''file_location = 'C:/Users/ahmad/Desktop/lux/GOM_seed_dataset/'
 for f in os.listdir(file_location):
     if 'grand_obstruction_matrix' in f:
         os.remove(file_location + f)
 print('All existing Grand Obstruction Matrices deleted!')
-
+'''
 # Call the function to create and save #N GOMs
 number_of_matrices = 500
 create_and_save_grand_obstruction_matrix(number_of_matrices)
